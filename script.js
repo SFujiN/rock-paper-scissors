644ab6d8c578f85282a55354b6706a60c287da44
SFujiN <d0n4lddanghuynh@gmail.com>
2023-02-25 23:09:24 -0600

    Update script.js

--- script.js
+++ script.js
@@ -27,9 +27,28 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
+    let playerScore = 0;
+    let computerScore = 0;
     for (let i = 0; i < 5; i++) {
         let playerSelection = prompt('Enter your choice (Rock, Paper, or Scissors): ');
-        console.log(playRound(playerSelection, getComputerChoice()));
+        let result = playRound(playerSelection, getComputerChoice());
+        if (result.split('!')[0] === 'You Win') {
+            playerScore++;
+        }
+        else if (result.split('!')[0] === 'You Lose') {
+            computerScore++;
+        }
+        console.log(result);
+        console.log(`playerScore: ${playerScore} | computerScore: ${computerScore}`);
+    }
+    if (playerScore > computerScore) {
+        console.log(`Winner! You won ${playerScore} out of 5 games`);
+    }
+    else if (playerScore < computerScore) {
+        console.log(`Loser! Computer won ${computerScore} out of 5 games`);
+    }
+    else {
+        console.log(`Tie! You both won ${playerScore} out of 5 games`)
     }
 }
 
